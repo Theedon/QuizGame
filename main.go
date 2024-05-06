8e794376ffe001ae2ae9725b8fb74c8023ecab2d
Theedon <faniyantoyin@gmail.com>
2024-05-06 23:18:36 +0100

    feat: solution for part 1

--- main.go
+++ main.go
@@ -1,7 +1,66 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+)
 
-func main (){
-	fmt.Println("Hello World")
+var scanner *bufio.Scanner = bufio.NewScanner(os.Stdin)
+
+func readFile(filePath *string, score *int) {
+	usageString := "input the file path of the csv file"
+	filePtr := flag.String("file", *filePath, usageString)
+
+	file, err := os.Open(*filePtr)
+
+	if err != nil {
+		log.Fatalf("Error while reading the %v file\n %v\n", filePath, err)
+	}
+
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	records, err := reader.ReadAll()
+
+	if err != nil {
+		fmt.Println("Error reading records", err)
+	}
+
+	for _, record := range records {
+		scoreQuestion(record, score)
+	}
+
+	printScore(*score, len(records))
+
+
+
+}
+
+func scoreQuestion(record []string, score *int) {
+
+	fmt.Printf("%v = ", record[0])
+	scanner.Scan()
+	text := scanner.Text()
+	if text == record[1] {
+		*score++
+	}
+}
+
+func printScore(score int, len int) {
+
+	perc := float64(score) / float64(len) * 100
+
+	fmt.Printf("\nFinal Score = %v\nTest Percentage = %.2f\n", score, perc)
+}
+
+func main() {
+	var filePath string = "problems.csv"
+	var score int = 0
+
+	readFile(&filePath, &score)
+
 }

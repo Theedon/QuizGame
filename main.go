24971b53c493566a5c4f5811b0293dcca9f39787
Theedon <faniyantoyin@gmail.com>
2024-05-07 13:12:14 +0100

    feat: add timer to quiz

--- main.go
+++ main.go
@@ -7,9 +7,11 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 )
 
 var scanner *bufio.Scanner = bufio.NewScanner(os.Stdin)
+var quit chan bool = make(chan bool)
 
 func readFile(filePath *string, score *int) {
 	usageString := "input the file path of the csv file"
@@ -31,12 +33,16 @@ func readFile(filePath *string, score *int) {
 	}
 
 	for _, record := range records {
-		scoreQuestion(record, score)
+		select {
+		case <-quit:
+			fmt.Println("Time out and Exiting")
+			os.Exit(0)
+		default:
+			scoreQuestion(record, score)
+		}
 	}
 
 	printScore(*score, len(records))
-
-
 
 }
 
@@ -57,10 +63,24 @@ func printScore(score int, len int) {
 	fmt.Printf("\nFinal Score = %v\nTest Percentage = %.2f\n", score, perc)
 }
 
+func timer(quit chan bool) {
+	timeout := 30
+	time.Sleep(time.Duration(timeout) * time.Second)
+	quit <- true
+}
+
 func main() {
+
 	var filePath string = "problems.csv"
 	var score int = 0
 
+	go timer(quit)
 	readFile(&filePath, &score)
+
+	select {
+	case <-quit:
+		fmt.Println("Time up")
+		os.Exit(0)
+	}
 
 }
